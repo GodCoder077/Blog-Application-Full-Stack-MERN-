7d3e9368bc3e0254989267742905785161a16ecc
Rakshith-Codr-77 <rakshitha24cs@rnsit.ac.in>
2025-08-30 21:54:54 +0530

    Initial Commit

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,109 @@
+import { User } from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export const register = async(req, res) => {
+    try {
+        const {firstName, lastName, email, password} = req.body;
+        if(!firstName || !lastName || !email || !password){
+            return res.status(400).json({
+                success: false,
+                message: "All fields are required"
+            });
+        }
+        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+        if(!emailRegex.test(email)){
+            return res.status(400).json({
+                success: false,
+                message: "Invalid email"
+            });
+        }
+        if(password.length < 6){
+            return res.status(400).json({
+                success: false,
+                message: "Password must be at least 6 characters long"
+            });
+        }
+
+        const existingUserbyEmail = await User.findOne({email:email});
+        if(existingUserbyEmail){
+            return res.status(400).json({
+                success: false,
+                message: "Email already exists"
+            });
+        }
+        console.log("REQ BODY ===>", req.body);
+        const hashPassword = await bcrypt.hash(password, 10)
+        await User.create({
+            firstName,
+            lastName,
+            email,
+            password: hashPassword
+        })
+        return res.status(201).json({
+            success: true,
+            message: "Account created successfully"
+        })
+    
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: "Failed to register"
+        })
+        
+    }
+}
+
+export const login = async(req, res) => {
+    try {
+        const {email, password} = req.body;
+        if(!email || !password){
+            return res.status(400).json({
+                success: false,
+                message:"All fields are required"
+            })
+        }
+        let user = await User.findOne({email: email})
+        if(!user){
+            return res.status(400).json({
+                success: false,
+                message: "Incorrect email or Password"
+            })
+        }
+        const isPasswordValid = await bcrypt.compare(password, user.password)
+        if(!isPasswordValid){
+            return res.status(400).json({
+                success: false,
+                message: "Invalid Credentials"
+            })
+        }
+
+
+        const token = await jwt.sign({userId : user._id}, process.env.SECRET_KEY,{expiresIn: "1d"})
+        return res.status(200).cookie("token", token, {maxAge: 1*24*60*60*1000, httpsOnly : true, sameSite:"strict"}).json({
+            success : true,
+            message : `Welcome Back ${user.firstName}`,
+            user
+        })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: "Failed to login"
+        })
+    }
+}
+
+export const logout = async(_, res) => {
+    try {
+        return res.status(200).cookie("token", "", {maxAge : 0}).json({
+            success: true,
+            message: "Logged out successfully"
+        })
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,92 @@
+import React from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import Logo from '../assets/logo.png'
+import { Input } from './ui/input'
+import { Button } from './ui/button'
+import { Search } from 'lucide-react'
+import { FaMoon, FaSun } from 'react-icons/fa'
+import { Avatar } from './ui/avatar'
+import { AvatarFallback, AvatarImage } from './ui/avatar'
+import { useDispatch, useSelector } from 'react-redux'
+import { toggleTheme } from '../redux/themeSlice'
+import { toast } from 'sonner'
+import axios from 'axios'
+import { setUser } from '../redux/authSlice'
+
+const Navbar = () => {
+    const {user} = useSelector(store => store.auth);
+    const {theme} = useSelector(store => store.theme)
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const logoutHandler = async (e) => {
+        try {
+            const res = await axios.get(`http://localhost:8000/api/v1/user/logout`, {withCredentials: true})
+            if(res.data.success){
+                navigate('/login')
+                dispatch(setUser(null))
+                toast.success(res.data.message)
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error(error.response?.data?.message || error.message);
+        }
+    }
+    return (
+        <div className='py-2 fixed w-full dark:bg-gray-800 dark:border-b-gray-600 border-b-gray-300 border-2 bg-white z-50'>
+            <div className='max-w-7xl mx-auto flex justify-between items-center px-4 md:px-0'>
+                {/* logo section */}
+                <div className='flex items-center gap-7'>
+                    <Link to={'/'}>
+                        <div className='flex gap-2 items-center' >
+                            <img src={Logo} className='w-7 h-7 md:w-10 md:h-10 dark:invert' />
+                            <h1 className='font-bold text-3xl md:text-4xl'>Inkspire</h1>
+                        </div>
+                    </Link>
+                    <div className='relative hidden md:block'>
+                        <Input
+                            type="text"
+                            placeholder="Search..."
+                            className="border border-gray-700 dark:bg-gray-900 bg-gray-300 w-[300px] hidden md:block"
+                        />
+                        <Button className="absolute right-0 top-0 cursor-pointer">
+                            <Search />
+                        </Button>
+                    </div>
+                </div>
+                {/* Nav section */}
+                <nav className='flex md:gap-7 gap-4 items-center'>
+                    <ul className='hidden md:flex gap-7 items-center text-xl font-semibold'>
+                        <Link to={'/'}><li>Home</li></Link>
+                        <Link to={'/blogs'}><li>Blogs</li></Link>
+                        <Link to={'/about'}><li>About</li></Link>
+                    </ul>
+                    <div className='flex'>
+                        <Button className='cursor-pointer' onClick={() => dispatch(toggleTheme())}>
+                            {
+                                theme === 'light' ? <FaMoon /> : <FaSun />
+                            }
+                            </Button>
+                        {
+                            user ? <div className='ml-7 flex gap-3 items-center'>
+                                <Avatar>
+                                    {/* <AvatarImage src={user.profilePicture} alt={user.name} />
+                                    <AvatarFallback>{user.name}</AvatarFallback> */}
+                                    <AvatarImage src="https://github.com/shadcn.png" />
+                                    <AvatarFallback>CN</AvatarFallback>
+                                </Avatar>
+                                <Button onClick={logoutHandler} className='cursor-pointer'>Logout</Button>
+                            </div> : <div className='ml-7 md:flex gap-2'>
+                                <Link to={'/login'}><Button className='cursor-pointer'>Login</Button></Link>
+                                <Link className='hidden md:block' to={'/signup'}><Button className='cursor-pointer'>SignUp</Button></Link>
+
+                            </div>
+                        }
+                    </div>
+                </nav>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
